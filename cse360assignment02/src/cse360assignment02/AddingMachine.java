40b0b7694c833d44382c65bfadde6d5cf35f0bf1
Jianlei Jiang <jjiang70@asu.edu>
2020-10-05 18:43:01 -0700

    Without methods

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total; 
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () { // return total 
+    return 0;
+  }
+  
+  public void add (int value) { // add value to total
+  }
+
+  public void subtract (int value) { // remove value from total
+  }
+
+  public String toString () { // print out "memory" using a string
+    return "";
+  }
+
+  public void clear() { // remove "memory" by resetting string
+  }
+}
